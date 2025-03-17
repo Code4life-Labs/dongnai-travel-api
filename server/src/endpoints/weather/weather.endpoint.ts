dbecbd99416ed130552b12a175125b1a7dc596d3
ThaiAnhDuc02 <thaiducdev@gmail.com>
2025-03-17 11:47:02 +0700

    update weather

--- server/src/endpoints/weather/weather.endpoint.ts
+++ server/src/endpoints/weather/weather.endpoint.ts
@@ -1,14 +1,47 @@
 // Import classes
 import { Endpoints } from "src/classes/Endpoints";
 
+// Import database
+import db from "src/databases/dongnaitravel";
+
+// Import helpers
+import getDongNaiWeather from "src/helpers/weather/endpoints/get-dongnai-weather";
+import getLocationWeather from "src/helpers/weather/endpoints/get-location-weather";
+
+// Import services
+import { AuthMiddlewares } from "src/services/auth/middlewares";
+
+// Import types
+import type { DongNaiTravelModelsType } from "src/databases/dongnaitravel";
+
 const weatherEndpoints = new Endpoints("weather");
+let DNTModels: DongNaiTravelModelsType;
+
+db().then((models) => {
+  DNTModels = models;
+});
 
 // Add your handlers here
 /**
- * Allow user get information of weather
+ * Get Dong Nai weather
  */
-weatherEndpoints.createHandler("").get((req, res) => {
-  return "Hello from weather root endpoint";
-});
+weatherEndpoints
+  .createHandler("/dongnai")
+  .use(AuthMiddlewares.allowGuest)
+  .use(AuthMiddlewares.checkToken)
+  .get(async (req, res, o) => {
+    return getDongNaiWeather(DNTModels, req, res, o);
+  });
+
+/**
+ * Get weather by location
+ */
+weatherEndpoints
+  .createHandler("/location")
+  .use(AuthMiddlewares.allowGuest)
+  .use(AuthMiddlewares.checkToken)
+  .get(async (req, res, o) => {
+    return getLocationWeather(DNTModels, req, res, o);
+  });
 
 export default weatherEndpoints;
